11e5446802a3ecb88ed7bc0a595b047371693279
root <admin@red2fred2.com>
2017-10-11 11:15:39 -0500

    edited css compiling for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,47 @@
-const gulp   = require('gulp'),
-      ejs    = require('gulp-ejs'),
-      rename = require('gulp-rename'),
-      using  = require('gulp-using'),
-      scss   = require('gulp-scss'),
+const gulp          = require('gulp'),
+      ejs           = require('gulp-ejs'),
+      rename        = require('gulp-rename'),
+      sass          = require('gulp-sass'),
+      cssmin        = require('gulp-cssmin'),
+      htmlmin       = require('gulp-htmlmin'),
       renderingData = require('./pages/renderingData.json')
 
 //ejs
-gulp.src('./pages/*.ejs')
-.pipe(
-  ejs(
-    {data:renderingData}
+gulp.task('ejs', function() {
+  gulp.src('./pages/*.ejs')
+  .pipe(
+    ejs({data:renderingData})
   )
-)
-.pipe(
-  rename(function (path) {
-    path.extname = '.html'
-  })
-)
-.pipe(
-  gulp.dest('./rendered')
-)
+  .pipe(
+    htmlmin({collapseWhitespace: true})
+  )
+  .pipe(
+    rename(function(path) {
+      path.extname = '.min.html'
+    })
+  )
+  .pipe(
+    gulp.dest('./rendered')
+  )
+})
 
 //scss
-gulp.src('./style/*.scss')
-.pipe(
-  scss({'bundleExec': true})
-)
-.pipe(
-  gulp.dest('./rendered')
-)
+gulp.task('scss', function() {
+  gulp.src('./style/*.scss')
+	.pipe(
+    sass({style:'expanded'})
+  )
+  .pipe(
+    cssmin()
+  )
+	.pipe(
+    rename(function(path) {
+      path.extname = '.min.css'
+    })
+  )
+	.pipe(
+    gulp.dest('./rendered')
+  )
+})
+
+gulp.task('default', ['ejs', 'scss'])
