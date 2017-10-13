5a7c2c2dab14ee27348f33e39e8e2292622770fc
red2fred2 <admin@red2fred2.com>
2017-10-13 12:44:24 -0500

    added css and js inlining to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
 const gulp          = require('gulp'),
+      del           = require('del'),
       ejs           = require('gulp-ejs'),
       rename        = require('gulp-rename'),
       sass          = require('gulp-sass'),
       cssmin        = require('gulp-cssmin'),
       htmlmin       = require('gulp-htmlmin'),
+      inline        = require('gulp-inline'),
+      babel         = require('gulp-babel'),
+	    wait          = require('gulp-wait'),
+      uglify        = require('gulp-uglify'),
       renderingData = require('./pages/renderingData.json')
 
+//clean build folder
+gulp.task('clean', function() {
+  del('./rendered/**')
+})
+
 //ejs
-gulp.task('ejs', function() {
-  gulp.src('./pages/*.ejs')
+gulp.task('ejs', ['clean'], function() {
+  gulp.src('./pages/daily.ejs')
   .pipe(
     ejs({data:renderingData})
   )
   .pipe(
-    htmlmin({collapseWhitespace: true})
-  )
-  .pipe(
     rename(function(path) {
-      path.extname = '.min.html'
+      path.extname = '.html'
     })
   )
   .pipe(
@@ -26,7 +33,7 @@ gulp.task('ejs', function() {
 })
 
 //scss
-gulp.task('scss', function() {
+gulp.task('scss', ['ejs'], function() {
   gulp.src('./style/*.scss')
 	.pipe(
     sass({style:'expanded'})
@@ -44,4 +51,35 @@ gulp.task('scss', function() {
   )
 })
 
-gulp.task('default', ['ejs', 'scss'])
+//babel
+gulp.task('babel', ['scss'], function() {
+	gulp.src('./scripts/*.js')
+	.pipe(
+    babel()
+	)
+	.pipe(
+    uglify()
+	)
+	.pipe(
+	  gulp.dest('./rendered')
+	)
+})
+
+//inline
+gulp.task('inline', ['babel'], function() {
+	gulp.src('./rendered/daily.html')
+	.pipe(
+	  wait(200)
+	)
+	.pipe(
+	  inline()
+	)
+	.pipe(
+    htmlmin({collapseWhitespace: true})
+  )
+	.pipe(
+    gulp.dest('./rendered')
+  )
+})
+
+gulp.task('default', ['inline'])
